643c4b0e522c7ca0eedeff790629ff1bf4af9886
jelle66 <jellemh@gmail.com>
2019-05-20 10:07:04 +0200

    removed broken demo

--- OnethinxExampleProjectsModus11/HelloWorld/HelloWorld_mainapp/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Users/Gebruiker/Desktop/HelloWorld/psoc6pdl_Cortex-M4/Debug/libpsoc6pdl_Cortex-M4.a
-
-LIBS :=
-
